cf0a5067db95290968141bd23cc5aaa440f3b35c
Juan Manuel Gaitan <35692784+jmgaitan@users.noreply.github.com>
2019-06-30 18:36:07 -0300

    Add files via upload

--- 1 Variables y Operadores/diccionarios.dart
+++ 1 Variables y Operadores/diccionarios.dart
@@ -1,3 +1,4 @@
+<<<<<<< HEAD:1 Variables y Operadores/diccionarios.dart
 main(List<String> args) {
 Map<int, dynamic> map;
 map={1:'rojo',2:'verde'};// arma de datos de tipo JSON
@@ -22,3 +23,29 @@ print(map.length);  // Saber el tamaño del json
 
 
 }
+=======
+main(List<String> args) {
+Map<int, dynamic> map;
+map={1:'rojo',2:'verde'};// arma de datos de tipo JSON
+
+
+print(map);
+
+map[3]='azul';
+
+print(map);
+
+map[4] =100 ; 
+print(map);
+
+print(map.length);
+map.remove(4);
+print(map);
+print(map.isEmpty); // Saber si tiene datos o no
+print(map.length);  // Saber el tamaño del json
+
+
+
+
+}
+>>>>>>> 74a1349ffcc70a1b8bae59e1fec6b5d9969af6f5:1 Variables y Operadores/diccionarios.dart
--- 1 Variables y Operadores/string-numbers-boolean.dart
+++ 1 Variables y Operadores/string-numbers-boolean.dart
@@ -1,3 +1,4 @@
+<<<<<<< HEAD:1 Variables y Operadores/string-numbers-boolean.dart
 main(List<String> arguments){
   int valInt = 1;
   double valDouble = 1 ; 
@@ -20,4 +21,28 @@ main(List<String> arguments){
   print('$valBool + $valString'); //concatenacion por (+) de dos variables
 
   print(valString.isEmpty);// con esto sabemos si contiene un valor o no
+=======
+main(List<String> arguments){
+  int valInt = 1;
+  double valDouble = 1 ; 
+  String valString = 'hola';
+  bool valBool = true;
+
+//concatenacion de spring
+
+  print('''$valString mi nombre es Juan Manuel
+
+
+  guarda el salto de linea definido''');
+  print("98232       \n salto de linea dos");
+  print(valInt);
+  print(valDouble);
+  print(valString);
+  print(valBool);
+//en dart todo es una clase
+  print('$valString mi nombre es JuanMa'); //concatenacion con una variable
+  print('$valBool + $valString'); //concatenacion por (+) de dos variables
+
+  print(valString.isEmpty);// con esto sabemos si contiene un valor o no
+>>>>>>> 74a1349ffcc70a1b8bae59e1fec6b5d9969af6f5:1 Variables y Operadores/string-numbers-boolean.dart
 }
